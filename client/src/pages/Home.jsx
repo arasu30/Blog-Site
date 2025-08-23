08ac184dd56cffac86964e6a0710b993431f768c
Ilavarasu Thevar <arasuthevar01@gmail.com>
2025-08-24 05:07:50 +0530

    Added header and newsletter

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Header from '../components/Header'
+import Newsletter from '../components/Newsletter'
 
 const Home = () => {
   return (
-     <>
+    <>
       <Navbar/>
+      <Header/>
+      
+      <Newsletter />
       
     </>
   )
