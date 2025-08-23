034a808a3c40bd250d92f1426de6215db5c829d0
Ilavarasu Thevar <arasuthevar01@gmail.com>
2025-08-24 05:18:59 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Blog from './pages/Blog.jsx'
+import 'quill/dist/quill.snow.css'
+import {Toaster} from 'react-hot-toast'
+import { useAppContext } from './context/AppContext'
+
 
 const App = () => {
+   const {token} = useAppContext()
 return (
     <div>
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
+        <Route path='/blog/:id' element={<Blog/>} />
       </Routes>
     </div>
   )
