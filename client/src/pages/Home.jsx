eebd49fa55451d54bc61d2e2fffce6b91dab2001
Ilavarasu Thevar <arasuthevar01@gmail.com>
2025-08-24 05:34:16 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
-    <>
+     <>
       <Navbar/>
       <Header/>
-      
       <Newsletter />
-      
+      <Footer />
     </>
   )
 }
-
 export default Home
