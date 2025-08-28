c02c108b78d775673f5d366ac07ef0581b376c84
Ilavarasu Thevar <arasuthevar01@gmail.com>
2025-08-28 06:07:38 +0530

    Fixed approved status in comments

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -43,7 +43,12 @@ const Comments = () => {
                     <th scope="col" className="px-6 py-3"> Action </th>
                 </tr>
             </thead>
-            
+            <tbody>
+                {comments.filter((comment)=>{
+                    if(filter === "Approved") return comment.isApproved === true;
+                    return comment.isApproved === false;
+                }).map((comment, index)=> <CommentTableItem key={comment._id} comment={comment} index={index + 1} fetchComments={fetchComments} />)}
+            </tbody>
         </table>
       </div>
     </div>
