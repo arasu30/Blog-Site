9cd43b6ea2ab39e9a3469852685c3b5416941284
Ilavarasu Thevar <arasuthevar01@gmail.com>
2025-08-30 06:05:59 +0530

    final changes

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -51,3 +51,22 @@ export const getDashboard = async (req, res) =>{
     }
 }
 
+export const deleteCommentById = async (req, res) =>{
+    try {
+        const {id} = req.body;
+        await Comment.findByIdAndDelete(id);
+        res.json({success: true, message:"Comment deleted successfully" })
+    } catch (error) {
+       res.json({success: false, message: error.message}) 
+    }
+}
+
+export const approveCommentById = async (req, res) =>{
+    try {
+        const {id} = req.body;
+        await Comment.findByIdAndUpdate(id, {isApproved: true});
+        res.json({success: true, message:"Comment approved successfully" })
+    } catch (error) {
+       res.json({success: false, message: error.message}) 
+    }
+}
